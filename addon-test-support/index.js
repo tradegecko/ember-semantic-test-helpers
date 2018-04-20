e9cd0751c7cf4c4275f057e19de1bd5facc9b3d4
billybonks <sebastienstettler@gmail.com>
2018-04-20 15:21:58 +0800

    rename to js

--- addon-test-support/index.js
+++ addon-test-support/index.js
@@ -2,12 +2,10 @@ import fillIn from './fillIn';
 import select from './select';
 import toggle from './toggle';
 import click from './click';
-import trackRequests from './request-tracker';
 
-export default {
+export {
   fillIn,
   select,
   toggle,
   click,
-  trackRequests
 };
