91fd983896f60b0f8352258f895a3028835b71f5
billybonks <sebastienstettler@gmail.com>
2018-04-21 00:33:02 +0800

    rudemitary spec compliant Text Alternative Computation
    https://www.w3.org/TR/accname-1.1/#mapping_additional_nd_te

--- addon-test-support/utils/find-label.js
+++ addon-test-support/utils/find-label.js
@@ -1,40 +1,45 @@
 import { findAll, find } from "@ember/test-helpers";
 
 export default function findLabel(labelText) {
-  let label = findByInnerText(labelText);
-  if(!label){
-    label = findByAriaLabelledBy(labelText);
-  }
-  if(!label){
-    label = findByAriaLabel(labelText);
-  }
-  return label;
-}
+  let label = null
+  let computedLabels = findAll('[aria-labelledby],[aria-label], [title], input[alt][type="image"], label[for]').map( (element) => {
+    //if is a label and target does not have aria then it is a percived label
+    if(element.tagName === 'LABEL' && element.attributes.for &&  element.attributes.for.value){
+      let control = find('#'+element.attributes.for.value);
+      if(!control.attributes['aria-label'] && !control.attributes['aria-labelledby']){
+        label = element.innerText;
+      }
+     }
 
-function findByAriaLabelledBy(labelText){
-  let label = findAll('[aria-labelledby]').map( (element) => {
-      return {label:element.attributes['aria-labelledby'].value, element}
-  }).map( (elementHash) => {
-    let label = elementHash.label.split(' ').map( (id) => {
-      return find(`#${id}`).innerText
-    }).join(' ')
-    return {label, element:elementHash.element}
-  }).find( (elementHash) => {
-     return elementHash.label === labelText
+     //first travese one depth of lablledby
+     if(!label && element.attributes['aria-labelledby'] && element.attributes['aria-labelledby'].value){
+       let ids = element.attributes['aria-labelledby'].value;
+       label = ids.split(' ').map( (id) => {
+         return find(`#${id}`).innerText
+       }).join(' ')
+     }
+
+     //then aria-label
+     if(!label && element.attributes['aria-label'] && element.attributes['aria-label'].value){
+       label = element.attributes['aria-label'].value;
+     }
+
+     //find rare case of type=image and alt
+     if(!label && element.attributes.type && element.attributes.type.value === "image" && element.attributes.alt && element.attributes.alt.value){
+       label = element.attributes['title'].value;
+     }
+
+     //lastly use title
+     if(!label && element.attributes.title && element.attributes.title.value){
+       label = element.attributes['title'].value;
+     }
+
+     if(label){
+       return {label, element}
+     }
   });
-  if(label){
-    return label.element
+  let result = computedLabels.find( hash => { return hash.label === labelText });
+  if(result){
+    return result.element;
   }
-}
-
-function findByInnerText(labelText){
-  return findAll('label').find( label => { return label.innerText === labelText });
-}
-
-function findByAriaLabel(labelText){
-  return findAll('[aria-label]').find( control => {
-    if(!control.attributes['aria-labelledby']){
-      return control.attributes['aria-label'].value === labelText
-    }
-  });
 }
