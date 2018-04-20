964abe695c22137ffb3a1c65ee51197f76b01600
billybonks <sebastienstettler@gmail.com>
2018-04-20 22:06:25 +0800

    neaten findLabel Code

--- addon-test-support/utils/find-label.js
+++ addon-test-support/utils/find-label.js
@@ -1,28 +1,40 @@
 import { findAll, find } from "@ember/test-helpers";
 
 export default function findLabel(labelText) {
-  let label = findAll('label').find( label => { return label.innerText === labelText });
+  let label = findByInnerText(labelText);
   if(!label){
-    label = findAll('[aria-labelledby]').map( (element) => {
-        return {label:element.attributes['aria-labelledby'].value, element}
-    }).map( (elementHash) => {
-      let label = elementHash.label.split(' ').map( (id) => {
-        return find(`#${id}`).innerText
-      }).join(' ')
-      return {label, element:elementHash.element}
-    }).find( (elementHash) => {
-       return elementHash.label === labelText
-    });
-    if(label){
-      label = label.element
-    }
+    label = findByAriaLabelledBy(labelText);
   }
   if(!label){
-    label = findAll('[aria-label]').find( control => {
-      if(!control.attributes['aria-labelledby']){
-        return control.attributes['aria-label'].value === labelText
-      }
-    });
+    label = findByAriaLabel(labelText);
   }
   return label;
+}
+
+function findByAriaLabelledBy(labelText){
+  let label = findAll('[aria-labelledby]').map( (element) => {
+      return {label:element.attributes['aria-labelledby'].value, element}
+  }).map( (elementHash) => {
+    let label = elementHash.label.split(' ').map( (id) => {
+      return find(`#${id}`).innerText
+    }).join(' ')
+    return {label, element:elementHash.element}
+  }).find( (elementHash) => {
+     return elementHash.label === labelText
+  });
+  if(label){
+    return label.element
+  }
+}
+
+function findByInnerText(labelText){
+  return findAll('label').find( label => { return label.innerText === labelText });
+}
+
+function findByAriaLabel(labelText){
+  return findAll('[aria-label]').find( control => {
+    if(!control.attributes['aria-labelledby']){
+      return control.attributes['aria-label'].value === labelText
+    }
+  });
 }
