b829620ca3ef087f2aac9d48192fe3a64f608eea
billybonks <sebastienstettler@gmail.com>
2018-04-22 17:17:17 +0800

    remove find label

--- addon-test-support/index.js
+++ addon-test-support/index.js
@@ -2,7 +2,6 @@ import fillIn from './fillIn';
 import select from './select';
 import toggle from './toggle';
 import click from './click';
-import findLabel from './utils/find-label';
 import findControl from './utils/find-control';
 import findButton from './utils/find-button';
 
@@ -11,7 +10,6 @@ export {
   select,
   toggle,
   click,
-  findLabel,
   findButton,
   findControl,
 };
