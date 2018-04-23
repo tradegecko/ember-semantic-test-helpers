d8e895db4becd44434be873eeed73ce1d7ac4db7
billybonks <sebastienstettler@gmail.com>
2018-04-23 10:22:21 +0800

    notify use when they try to use a value

--- addon-test-support/utils/fillin-select.js
+++ addon-test-support/utils/fillin-select.js
@@ -2,10 +2,16 @@ import { findAll, fillIn } from '@ember/test-helpers';
 
 export default async function(control, value){
   let options = await findAll(`#${control.attributes.id.value} option`);
-  let option =options.find((option) => {
+  let option = options.find((option) => {
     return option.innerText === value
   })
   if(!option){
+    option = options.find((option) => {
+      return option.attributes.value.value == value
+    })
+    if(option){
+      throw new Error(`You tried to fill in using value "${value}" instead of the semantic label "${option.innerText}"`)
+    }
     throw new Error(`could not find option ${value}`)
   }
   return fillIn(control, option.attributes.value.value);
--- tests/integration/components/select-test.js
+++ tests/integration/components/select-test.js
@@ -22,4 +22,22 @@ module('Integration | Helper | select', function(hooks) {
     await select('Location', 'aaa');
     assert.equal(find('#control').value,2)
   });
+
+  test('fills in using label', async function(assert) {
+    // Template block usage:
+    await render(hbs`
+      <label for='control'>Location</label>
+      <select id="control">
+        <option value="1">qqq</option>
+        <option value="2">aaa</option>
+        <option value="3">bbb</option>
+      </select>
+    `);
+    assert.notEqual(find('#control').value,2)
+    try {
+      await select('Location', 2);
+    } catch(e){
+      assert.equal(e.message, 'You tried to fill in using value "2" instead of the semantic label "aaa"')
+    }
+  });
 })
