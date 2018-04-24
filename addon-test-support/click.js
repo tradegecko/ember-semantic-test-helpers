ce2a3c29cae84d81d688f1fedd36610c3ecd70fd
billybonks <sebastienstettler@gmail.com>
2018-04-25 00:25:17 +0800

    add rudimentary support for power select

--- addon-test-support/click.js
+++ addon-test-support/click.js
@@ -2,8 +2,8 @@ import { click } from '@ember/test-helpers';
 import findButton from './find-button';
 
 export default async function clickByLabel(text) {
-  let element = findButton(text);
 
+  let element = await findButton(text);
   if (!element) {
     throw new Error(`Could not find a button containing "${text}"`);
   }
