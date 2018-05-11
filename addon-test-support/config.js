a1d40ad18020c6531a532c66fba64650b2029506
billybonks <sebastienstettler@gmail.com>
2018-05-11 18:15:48 +0800

    update errors

--- addon-test-support/config.js
+++ addon-test-support/config.js
@@ -1,5 +1,3 @@
-import AmbiguousLabel from './errors/ambiguous-label';
-import MissingObject from './errors/missing-object';
 import InvalidFor from './errors/invalid-for';
 import PercivedByName from './errors/percived-by-name';
 import findByAria from './dom/find-by-aria';
@@ -7,26 +5,26 @@ import findByLabel from './dom/find-by-label';
 import findByName from './dom/find-by-name';
 
 export default {
-  ambiguousLabel: 1,
   invalidFor: 1,
   percivedByName: 1,
-  ariaNotFound: 1
 }
 
 
 export let errors = {
-  ambiguousLabel: AmbiguousLabel,
   invalidFor: InvalidFor,
   percivedByName: PercivedByName,
-  ariaNotFound: MissingObject
 }
 
 export let buildMessage = function(error, type, labelText){
   switch(error){
+    case 'percivedByName' :
+      return `Control ${labelText} found through input name attribute`;
+    case 'invalidFor' :
+      return `Control ${labelText} found through invalid label for relationship`;
     case 'ambiguousLabel' :
-      return  `Multiple ${type} labelled ${labelText} where found`
+      return  `Multiple ${type} labelled ${labelText} where found`;
     case 'ariaNotFound' :
-        return `Could not find ${type} labelled '${labelText}'`
+      return `Could not find ${type} labelled '${labelText}'`;
   }
 }
 
