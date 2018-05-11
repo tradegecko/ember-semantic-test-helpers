44e157dd86097a67822fb16db2eceb1f466829c6
billybonks <sebastienstettler@gmail.com>
2018-05-11 18:16:40 +0800

    update tests

--- tests/integration/components/fallback-test.js
+++ tests/integration/components/fallback-test.js
@@ -5,9 +5,17 @@ import { module, test } from 'qunit';
 import { render } from '@ember/test-helpers';
 import { fillIn } from 'ember-semantic-test-helpers/test-support';
 import { find } from 'ember-test-helpers';
+import sinon from 'sinon';
 
 module('Integration | Helper | fallbacks', function(hooks) {
   setupRenderingTest(hooks);
+  hooks.beforeEach(function(){
+    this.sandbox = sinon.sandbox.create();
+  })
+
+  hooks.afterEach(function(){
+    this.sandbox.restore();
+  })
 
   module('invalidFor', function(hooks){
     hooks.beforeEach(async function() {
@@ -22,59 +30,35 @@ module('Integration | Helper | fallbacks', function(hooks) {
     module('level 0', function(hooks){
       enableErrors(hooks, {invalidFor:0});
       test('Does not fill input, produces an error', async function(assert){
-
+        try {
+          await fillIn('Location', 'awesomerable');
+        } catch(e){
+          assert.equal(e.message, 'Control Location found through invalid label for relationship')
+          assert.equal(e.constructor.name, 'InvalidFor')
+        }
       });
     });
 
     module('level 1', function(hooks){
       enableErrors(hooks, {invalidFor:1});
       test('Fills input and produces a warning', async function(assert){
+        let spy = this.sandbox.spy(console, 'warn');
         await fillIn('Location', 'awesomerable');
         let input = find('#control input');
         assert.equal(input.value, 'awesomerable');
+        assert.ok(spy.calledWith('Control Location found through invalid label for relationship'));
+        assert.ok(spy.calledOnce)
       });
     });
 
     module('level 2', function(hooks){
       enableErrors(hooks, {invalidFor:2});
       test('Fills input, silently', async function(assert){
-
-      });
-    });
-  });
-
-  module('ambiguousLabel', function(hooks){
-    hooks.beforeEach(async function() {
-
-      await render(hbs`
-        <label for='control'>Location</label>
-        {{input id="control" type="text"}}
-        <label for='control2'>Location</label>
-        {{input id="control2" type="text"}}
-      `);
-    });
-    module('level 0', function(hooks){
-      enableErrors(hooks, {ambiguousLabel:0});
-      test('Does not fill input, produces an error', async function(assert){
-
-      });
-    });
-
-    module('level 1', function(hooks){
-      enableErrors(hooks, {ambiguousLabel:1});
-      test('Fills in first input and produces a warning', async function(assert){
-        await fillIn('Location', 'awesome');
-        let input = find('#control');
-        assert.equal(input.value, 'awesome');
-        input = find('#control2');
-        assert.equal(input.value, '');
-      });
-    });
-
-    module('level 2', function(hooks){
-      enableErrors(hooks, {ambiguousLabel:2});
-      test('Fills in first input, silently', async function(assert){
-
+        let spy = this.sandbox.spy(console, 'warn');
+        await fillIn('Location', 'awesomerable');
+        let input = find('#control input');
+        assert.equal(input.value, 'awesomerable');
+        assert.equal(spy.called, false);
       });
     });
   });
@@ -89,23 +73,35 @@ module('Integration | Helper | fallbacks', function(hooks) {
     module('level 0', function(hooks){
       enableErrors(hooks, {percivedByName:0});
       test('Does not fill input, produces an error', async function(assert){
-
+        try {
+          await fillIn('Location', 'awesomerable');
+        } catch(e){
+          assert.equal(e.message, 'Control Location found through input name attribute')
+          assert.equal(e.constructor.name, 'PercivedByName')
+        }
       });
     });
 
-    module('level 1', function(hooks){
+    module('level 1', async function(hooks){
       enableErrors(hooks, {percivedByName:1});
       test('Fills input and produces a warning', async function(assert){
+        let spy = this.sandbox.spy(console, 'warn');
         await fillIn('Location', 'awesomerable');
-        let input = find('#control input');
+        let input = find('input');
         assert.equal(input.value, 'awesomerable');
-      });
+        assert.ok(spy.calledWith('Control Location found through input name attribute'));
+        assert.ok(spy.calledOnce)
+      })
     });
 
     module('level 2', function(hooks){
       enableErrors(hooks, {percivedByName:2});
       test('Fills input, silently', async function(assert){
-
+        let spy = this.sandbox.spy(console, 'warn');
+        await fillIn('Location', 'awesomerable');
+        let input = find('input');
+        assert.equal(input.value, 'awesomerable');
+        assert.equal(spy.called, false);
       });
     });
   });
