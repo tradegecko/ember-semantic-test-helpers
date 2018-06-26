5dad459ee3ffe3b9ea0089e4683706d443d079e3
billybonks <sebastienstettler@gmail.com>
2018-06-26 23:13:22 +0800

    easiest way to move finders to a differen't package

--- addon-test-support/definitions/selectors.js
+++
@@ -1,7 +0,0 @@
-import { button, text, toggle, select } from './types';
-
-export let buttonQuery = button.join(',');
-export let textQuery  = text.join(',');
-export let toggleQuery = toggle.join(',');
-export let selectQuery = select.join(',');
-export let formControlQuery = [textQuery, toggleQuery, selectQuery].join(',');
--- addon-test-support/finders/find-by-aria/index.js
+++
@@ -1,8 +0,0 @@
-import computeAria from './compute-aria';
-import findAll from './../helpers/findAll';
-
-export default function findByAria(selector, labelText) {
-  return findAll(selector).filter( (element) => {
-    return computeAria(element) === labelText.toLowerCase()
-  });
-}
