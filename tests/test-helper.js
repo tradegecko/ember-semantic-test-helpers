2dc01cc298a3dab6fa50df15a90875d64fdda2ce
billybonks <sebastienstettler@gmail.com>
2018-10-28 01:03:19 +0800

    need to do some config now

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -2,6 +2,12 @@ import Application from '../app';
 import config from '../config/environment';
 import { setApplication } from '@ember/test-helpers';
 import { start } from 'ember-qunit';
+import { configure } from 'semantic-dom-selectors';
+
+configure({
+  preset: 'default',
+  rootElementSelector: 'ember-testing'
+});
 
 setApplication(Application.create(config.APP));
 
