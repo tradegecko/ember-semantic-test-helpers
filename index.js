6bc5a78abb06db8a00f48a3fc45c132e91bbe5a4
Wong Pei Yi <wongpeiyi@gmail.com>
2020-04-01 19:56:57 +0800

    Simplify imports
    - Import directly instead of going through funnels
    - project.root is not always the same as the package root, e.g. when
    using yarn workspaces

--- index.js
+++ index.js
@@ -1,34 +1,14 @@
-'use strict';
-
-
 /* eslint-env node */
 'use strict';
-
-const Funnel = require('broccoli-funnel');
-const MergeTrees = require('broccoli-merge-trees');
 
 module.exports = {
   name: require('./package').name,
 
   included() {
     this._super.included.apply(this, arguments);
-    this.import('vendor/semantic-dom-selectors.js', { type: 'test' });
-    this.import('vendor/qunit-semantic-assertions.js', { type: 'test' });
-  },
-
-  treeForVendor(vendorTree) {
-    let semanticTestHelperTree = new Funnel(`${this.project.root}/node_modules/semantic-dom-selectors/dist`, {
-      files: ['semantic-dom-selectors.js', 'semantic-dom-selectors.js.map'],
-    });
-    let qunitSemanticAssertionsTree = new Funnel(`${this.project.root}/node_modules/qunit-semantic-assertions/dist`, {
-      files: ['qunit-semantic-assertions.js', 'qunit-semantic-assertions.js.map'],
-    });
-    if(vendorTree){
-      //if tree is for app this is not null
-      return new MergeTrees([vendorTree, semanticTestHelperTree, qunitSemanticAssertionsTree]);
-    } else {
-      //if is an addon this is null
-      return new MergeTrees([qunitSemanticAssertionsTree,semanticTestHelperTree]);
-    }
-  },
+    this.import('node_modules/semantic-dom-selectors/dist/semantic-dom-selectors.js', { type: 'test' });
+    this.import('node_modules/semantic-dom-selectors/dist/semantic-dom-selectors.map', { type: 'test' });
+    this.import('node_modules/qunit-semantic-assertions/dist/qunit-semantic-assertions.js', { type: 'test' });
+    this.import('node_modules/qunit-semantic-assertions/dist/qunit-semantic-assertions.map', { type: 'test' });
+  }
 }
